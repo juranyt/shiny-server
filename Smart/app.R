7bf805004e508c7bc06ba2d8185ebdb9e9582d53
juranyt <35330505+juranyt@users.noreply.github.com>
2018-07-26 13:23:59 +0200

    Add files via upload
    updated dorting and font

--- Smart/app.R
+++ Smart/app.R
@@ -1,9 +1,9 @@
-.libPaths( c( .libPaths(), "/home/jirik/R/x86_64-pc-linux-gnu-library/3.4") )
-library("shiny")
-library("DT")
+library(xlsx)
+library(shiny)
+library(DT)
 source("load_smart_rating.R")
 
-ui <- fluidRow(
+ui <- fluidRow(includeCSS("www/styles.css"),
   DTOutput('tbl')
 )
 
@@ -15,8 +15,20 @@ server <- function(input, output) {
                            pageLength = nrow(df),
                            initComplete = JS("function(settings, json) {$(this.api().table().header()).css({'background-color' : '#e6ca49'});}"),
                            order = list(list(0, 'asc')),
-                           columnDefs = list(list(className = 'dt-center', targets = 1:9)))) %>% formatRound(columns = c(2:9),digits = 2)
-   output$tbl <- renderDT(dt)
+                           columnDefs = list(list(
+                             #className = 'dt-center', targets = 1:9),
+                             targets = 9, render = JS(
+                               "function(data, type, row, meta) {
+                                   if (type != 'display') {
+                                      return data;
+                                   }
+                                   var ratings = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'F'];
+                                   return (ratings[data]);
+                              }"
+                             )),
+                             list(
+                             targets = 1:9, className = 'dt-center')))) %>% formatRound(columns = c(2:9),digits = 2)
+  output$tbl <- renderDT(dt)
   
 }
 
--- Smart/load_smart_rating.R
+++ Smart/load_smart_rating.R
@@ -1,10 +1,14 @@
 load_smart_rating <- function(filePath){
-  library("xlsx")
+  library(xlsx)
   icos <- unlist(read.xlsx(filePath,sheetName = "master", startRow = 4, colIndex = 1, colClasses = "character", as.data.frame = FALSE))
   
   df <- read.xlsx(filePath,sheetName = "master",header = FALSE, startRow = 4, colIndex = c(12:20), colClasses = c(rep("numeric",8),"character"))
   df <- df[apply(df, 1, function(df) !all(is.na(df))),] # Delete trailing NA rows
   df <- cbind(icos,df)
+  colnames(df) <- c("ICO","IcoDrops","OhHeyMatty","CryptoBriefing","CruishCrypto","Hacked","ICOPantera","Bulk","SmartAvg","SmartRating")
+  mapping <- data.frame(SmartRating = c("A+","A","A-","B+","B","B-","C+","C","C-","F"), value = c(0:9))
+  df <- merge(x = df, y = mapping, by = "SmartRating", all.y = TRUE)
+  df <- df[,c(2:10,11)]
   colnames(df) <- c("ICO","IcoDrops","OhHeyMatty","CryptoBriefing","CruishCrypto","Hacked","ICOPantera","Bulk","SmartAvg","SmartRating")
   return(df)
 }
